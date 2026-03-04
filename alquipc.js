cba8f4c9a3c583f29a879823a3c94e0517eb4ea1
Juan Sebastian Velasquez <sebistrapo@gmail.com>
2026-03-04 07:07:28 -0500

    Validación de campos

--- alquipc.js
+++ alquipc.js
@@ -1,34 +1,46 @@
 const valor = 35000;
-let idCliente = Math.floor(Math.random() * 1000)
+let idCliente = Math.floor(Math.random() * 1000);
 
 // Dentro de la Ciudad ------------------------------------
 
 function DentroCiudad() {
-
     let name = document.getElementById("nombreDentroC").value;
     let days = parseInt(document.getElementById("diasDentroC").value);
     let can = parseInt(document.getElementById("cantidadDentroC").value);
 
-    if (name == "" || isNaN(days) || days < 1 || isNaN(can)) {
+    if (/^([0-9])*$/.test(name)) {
+        alert("El campo nombre no puede contener números");
+    }else {
+        if (name == "" || isNaN(days) || days < 1 || isNaN(can)) {
         alert("Debes llenar todos los Datos Correctamente");
-    } else {
-        if (can >= 2) {
-            
-            let total = days * valor * can;
-            localStorage.setItem("subTotal", total)
+        } else {
+            if (can >= 2) {
+                let total = days * valor * can;
+                localStorage.setItem("subTotal", total);
 
-            let recibo = "Recibo de " + name + "   ID: " + idCliente + "\n" + 
-                         "Servicio: Dentro de la Ciudad" + "\n" +
-                         "Días alquilados: " + days + "\n" + 
-                         "Computadores alquilados: " + can + "\n" + 
-                         "Total: $ " + total;
+                let recibo =
+                "Recibo de " +
+                name +
+                "   ID: " +
+                idCliente +
+                "\n" +
+                "Servicio: Dentro de la Ciudad" +
+                "\n" +
+                "Días alquilados: " +
+                days +
+                "\n" +
+                "Computadores alquilados: " +
+                can +
+                "\n" +
+                "Total: $ " +
+                total;
 
                 localStorage.setItem("total", recibo);
-                
-                window.location.href = "factura.html";
-        }else {
 
-            alert("Debes de Alquilar minimo 2 computadores")
+                window.location.href = "factura.html";
+            } else {
+                alert("Debes de Alquilar minimo 2 computadores");
+            }
         }
     }
 }
@@ -36,36 +48,50 @@ function DentroCiudad() {
 // Fuera de la Ciudad ------------------------------------
 
 function FueraCiudad() {
-
     let name = document.getElementById("nombreFueraC").value;
     let days = parseInt(document.getElementById("diasFueraC").value);
     let can = parseInt(document.getElementById("cantidadFueraC").value);
 
-    if (name == "" || isNaN(days) || days < 1 || isNaN(can)) {
-        alert("Debes llenar todos los Datos correctamente");
-    } else{
-        if (can >= 2) {
-
+    if (/^([0-9])*$/.test(name)) {
+        alert("El campo nombre no puede contener números");
+    } else {
+        if (name == "" || isNaN(days) || days < 1 || isNaN(can)) {
+            alert("Debes llenar todos los Datos correctamente");
+        } else {
+            if (can >= 2) {
             let subtotal = days * valor * can;
             let incremento = subtotal * 0.05;
             let total = subtotal + incremento;
-            localStorage.setItem("subTotal", total)
+            localStorage.setItem("subTotal", total);
 
-
-            let recibo = "Recibo de " + name + "   ID: " + idCliente +  "\n" +
-                        "Servicio: Fuera de la ciudad (+5%)\n" +
-                        "Días alquilados: " + days + "\n" +
-                        "Computadores alquilados: " + can + "\n" +
-                        "Subtotal: $" + subtotal + "\n" +
-                        "Incremento (5%): $" + incremento + "\n" +
-                        "Total: $" + total;
+            let recibo =
+                "Recibo de " +
+                name +
+                "   ID: " +
+                idCliente +
+                "\n" +
+                "Servicio: Fuera de la ciudad (+5%)\n" +
+                "Días alquilados: " +
+                days +
+                "\n" +
+                "Computadores alquilados: " +
+                can +
+                "\n" +
+                "Subtotal: $" +
+                subtotal +
+                "\n" +
+                "Incremento (5%): $" +
+                incremento +
+                "\n" +
+                "Total: $" +
+                total;
 
             localStorage.setItem("total", recibo);
 
             window.location.href = "factura.html";
-
-        } else {
-            alert("Debes alquilar mínimo 2 computadores");
+            } else {
+                alert("Debes alquilar mínimo 2 computadores");
+            }
         }
     }
 }
@@ -73,45 +99,61 @@ function FueraCiudad() {
 // Dentro del Establecimiento ----------------------------
 
 function DentroEstablecimiento() {
-
     let name = document.getElementById("nombreDentroE").value;
     let can = parseInt(document.getElementById("cantidadDentroE").value);
     let fecha = document.getElementById("diaAlquiler").value;
     let hora = document.getElementById("horaAlquier").value;
 
-
-    if (name == "" || fecha == "" || hora == "" || isNaN(can)) {
-        alert("Debes llenar todos los Datos")
-    }else {
-        if (can >= 2) {
-
-            let subtotal = valor * can;
-            let descuento = subtotal * 0.05;
-            let total = subtotal - descuento;
-            localStorage.setItem("subTotal", total)
-
-
-            let recibo = "Recibo de " + name + "   ID: " + idCliente +  "\n" +
-                        "Servicio: Dentro del establecimiento (-5%)\n" +
-                        "Fecha: " + fecha + "\n" +
-                        "Hora: " + hora + "\n" +
-                        "Computadores alquilados: " + can + "\n" +
-                        "Subtotal: $" + subtotal + "\n" +
-                        "Descuento (5%): $" + descuento + "\n" +
-                        "Total: $" + total;
-
-            localStorage.setItem("total", recibo);
-
-            window.location.href = "factura.html";
-
+    if (/^([0-9])*$/.test(name)) {
+        alert("El campo nombre no puede contener números");
+    } else {
+        if (name == "" || fecha == "" || hora == "" || isNaN(can)) {
+        alert("Debes llenar todos los Datos");
         } else {
-            alert("Debes alquilar mínimo 2 computadores");
+            if (can >= 2) {
+                let subtotal = valor * can;
+                let descuento = subtotal * 0.05;
+                let total = subtotal - descuento;
+                localStorage.setItem("subTotal", total);
+
+                let recibo =
+                "Recibo de " +
+                name +
+                "   ID: " +
+                idCliente +
+                "\n" +
+                "Servicio: Dentro del establecimiento (-5%)\n" +
+                "Fecha: " +
+                fecha +
+                "\n" +
+                "Hora: " +
+                hora +
+                "\n" +
+                "Computadores alquilados: " +
+                can +
+                "\n" +
+                "Subtotal: $" +
+                subtotal +
+                "\n" +
+                "Descuento (5%): $" +
+                descuento +
+                "\n" +
+                "Total: $" +
+                total;
+
+                localStorage.setItem("total", recibo);
+
+                window.location.href = "factura.html";
+            } else {
+                alert("Debes alquilar mínimo 2 computadores");
+            }
         }
     }
 }
 
-window.addEventListener("DOMContentLoaded", function() {
 
+
+window.addEventListener("DOMContentLoaded", function () {
     let recibo = document.getElementById("recibo2");
 
     if (recibo) {
@@ -123,27 +165,29 @@ window.addEventListener("DOMContentLoaded", function() {
 // Dias Adicionales
 
 function DiasAdicionales() {
-
     let recibo = document.getElementById("recibo2");
 
-    let diasExtra = prompt("¿Desea agregar días adicionales? \n(Ingrese número de dias o 0 para cancelar)");
+    let diasExtra = prompt(
+        "¿Desea agregar días adicionales? \n(Ingrese número de dias o 0 para cancelar)",
+    );
 
     if (diasExtra !== null) {
-
         diasExtra = parseInt(diasExtra);
 
         if (!isNaN(diasExtra) && diasExtra > 0) {
+        let subtotal = parseInt(localStorage.getItem("subTotal"));
 
-            let subtotal = parseInt(localStorage.getItem("subTotal"));
+        let costoExtra = diasExtra * valor;
 
-            let costoExtra = diasExtra * valor;
+        let nuevoTotal = subtotal + costoExtra;
 
-            let nuevoTotal = subtotal + costoExtra;
-
-            recibo.value += 
-                "\n\nDías adicionales: " + diasExtra +
-                "\nCosto adicional: $" + costoExtra +
-                "\nNuevo Total: $" + nuevoTotal;
+        recibo.value +=
+            "\n\nDías adicionales: " +
+            diasExtra +
+            "\nCosto adicional: $" +
+            costoExtra +
+            "\nNuevo Total: $" +
+            nuevoTotal;
         }
     }
 }
